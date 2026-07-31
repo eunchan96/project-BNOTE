d15aee67244260066739f55f1da6cd5cd87a7c41
eunchan <taegwon02@gmail.com>
2026-07-31 23:51:38 +0900

    feat: build gradle 갱신 (#38)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
 		applicationId = "com.chan.bnote"
 		minSdk = 24
 		targetSdk = 35
-		versionCode = 2
-		versionName = "1.1"
+		versionCode = 3
+		versionName = "1.2"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
