92e3762cd4cd463e576b46e0aef8f16020f4e364
eunchan96 <taegwon02@gmail.com>
2026-09-25 16:31:38 +0900

    chore: 버전업 v1.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
 		applicationId = "com.chan.bnote"
 		minSdk = 24
 		targetSdk = 35
-		versionCode = 11
-		versionName = "1.10"
+		versionCode = 12
+		versionName = "1.11"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
